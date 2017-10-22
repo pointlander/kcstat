8bdc277005f64b1e637f1ceeda9bd8826565a882
Andrew Snodgrass <git@pointlander.info>
2017-10-22 15:33:40 -0600

    First commit

---
+++ ga.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"math/rand"
+
+	ga "github.com/pointlander/go-galib"
+)
+
+type GACDFInitializer struct{}
+
+func (i *GACDFInitializer) InitPop(first ga.GAGenome, popsize int) (pop []ga.GAGenome) {
+	pop = make([]ga.GAGenome, popsize)
+	for x := 0; x < popsize; x++ {
+		genome := first.Copy().(*ga.GAFloat32Genome)
+		pop[x] = genome
+		for i := 0; i < 256; i++ {
+			offset := i * 256
+			for j := 0; j < 256; j++ {
+				genome.Gene[offset+j] = rand.Float32() * .1
+				if i == j {
+					genome.Gene[offset+j] = 1
+				}
+			}
+		}
+	}
+	return pop
+}
+
+func (i *GACDFInitializer) String() string { return "CDFInitializer" }
+
+type GABoundedGaussianMutator struct {
+	StdDev float64
+	Mean   float64
+}
+
+func NewGABoundedGaussianMutator(stddev float64, mean float64) *GABoundedGaussianMutator {
+	if stddev == 0 {
+		return nil
+	}
+	return &GABoundedGaussianMutator{StdDev: stddev, Mean: mean}
+}
+
+func (m GABoundedGaussianMutator) Mutate(a ga.GAGenome) ga.GAGenome {
+	switch a := a.(type) {
+	case *ga.GAFloatGenome:
+		n := a.Copy().(*ga.GAFloatGenome)
+		l := a.Len()
+		s := rand.Intn(l)
+		n.Gene[s] += rand.NormFloat64()*m.StdDev + m.Mean
+		if n.Gene[s] < n.Min {
+			n.Gene[s] = n.Min
+		} else if n.Gene[s] > n.Max {
+			n.Gene[s] = n.Max
+		}
+		return n
+	case *ga.GAFloat32Genome:
+		n := a.Copy().(*ga.GAFloat32Genome)
+		l := a.Len()
+		s := rand.Intn(l)
+		n.Gene[s] += float32(rand.NormFloat64()*m.StdDev + m.Mean)
+		if n.Gene[s] < n.Min {
+			n.Gene[s] = n.Min
+		} else if n.Gene[s] > n.Max {
+			n.Gene[s] = n.Max
+		}
+		return n
+	}
+
+	return nil
+}
+
+func (m GABoundedGaussianMutator) String() string { return "GABoundedGaussianMutator" }
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/pointlander/compress"
+	ga "github.com/pointlander/go-galib"
+)
+
+const (
+	// Symbols the number of symbols
+	Symbols = 256
+	// Width the width of the genome
+	Width = Symbols * Symbols
+	// Verify verify the fitness function
+	Verify = false
+)
+
+var data []byte
+
+func press(g *ga.GAFloat32Genome) float32 {
+	newCDF := func(size int) *compress.CDF {
+		if size != 256 {
+			panic("size is not 256")
+		}
+		cdf, mixin := make([]uint16, size+1), make([][]uint16, size)
+
+		sum := 0
+		for i := range cdf {
+			cdf[i] = uint16(sum)
+			sum += 32
+		}
+
+		for i := range mixin {
+			sum, m, offset, total := 0, make([]uint16, size+1), i*Symbols, float32(0)
+			for j := 0; j < Symbols; j++ {
+				total += g.Gene[offset+j]
+			}
+			for j := range m[:Symbols] {
+				m[j] = uint16(sum)
+				sum += int(1 + g.Gene[offset+j]*(compress.CDFScale-Symbols)/total)
+			}
+			m[Symbols] = compress.CDFScale
+			mixin[i] = m
+		}
+
+		return &compress.CDF{
+			CDF:    cdf,
+			Mixin:  mixin,
+			Verify: Verify,
+		}
+	}
+
+	input := make([]uint16, len(data))
+	for i := range data {
+		input[i] = uint16(data[i])
+	}
+	symbols, buffer := make(chan []uint16, 1), &bytes.Buffer{}
+	symbols <- input
+	close(symbols)
+	bits := compress.Coder16{Alphabit: 256, Input: symbols}.FilteredAdaptiveCoder(newCDF).Code(buffer)
+	return float32(bits)
+}
+
+func main() {
+	alice, err := ioutil.ReadFile("alice30.txt")
+	if err != nil {
+		panic(err)
+	}
+	data = alice
+
+	mutator := ga.NewMultiMutator()
+	msh := new(ga.GAShiftMutator)
+	msw := new(ga.GASwitchMutator)
+	gm := NewGABoundedGaussianMutator(0.3, 0)
+	mutator.Add(msh)
+	mutator.Add(msw)
+	mutator.Add(gm)
+
+	param := ga.GAParameter{
+		Initializer: new(GACDFInitializer),
+		Selector:    ga.NewGATournamentSelector(0.2, 5),
+		PMutate:     0.5,
+		PBreed:      0.2,
+		Breeder:     new(ga.GA2PointBreeder),
+		Mutator:     mutator,
+	}
+	gao := ga.NewGA(param)
+	gao.Parallel = true
+	gao.Init(200, ga.NewFloat32Genome(make([]float32, Width), press, 1, 0))
+
+	gao.OptimizeUntil(func(best ga.GAGenome) bool {
+		score := best.Score() / 8
+		fmt.Printf("best = %v\n", score)
+		return score < .3*float64(len(alice))
+	})
+}
